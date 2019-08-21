1441b260e51fda27e7674559c543999369131a8c
hoandreas <andreas_ho@hotmail.com>
2019-08-22 02:28:53 +0700

    Adding exercise 8 and 9

---
+++ exercise8w5.js
@@ -0,0 +1,9 @@
+function hapusSimbol(str) {
+    return str.replace(/[^a-z0-9]/g, '');
+  }
+  
+  // TEST CASES
+  console.log(hapusSimbol('test%$4aa')); // test4aa
+  console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+  console.log(hapusSimbol('ma@#k!an~')); // makan
+  console.log(hapusSimbol('coding')); // coding
